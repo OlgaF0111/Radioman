cd879a22aebed13ca85ed678bcec4968098924b6
Olga F <krasota1_00@mail.ru>
2023-03-22 13:44:44 +0700

    modification project

--- src/main/java/org/example/Radioman/radio/Radio.java
+++ src/main/java/org/example/Radioman/radio/Radio.java
@@ -1,33 +1,52 @@
 package org.example.Radioman.radio;
+
 public class Radio {
     private int radioStationNumber;
     private int soundVolume;
+    private int maxRadioStations;
+    private int maxVolumeLevel = 100;
+
+    //кол-во радиостанций по умолчанию
+    public Radio() {
+
+        maxRadioStations = 9;
+    }
+
+    // кол-во радиостанций всего
+    public Radio(int radioStationNumber) {
+
+        maxRadioStations = radioStationNumber - 1;
+    }
+
     // номер следующей станции
     public void nextStationNumber() {
-        if (radioStationNumber != 9) {
+        if (radioStationNumber != maxRadioStations) {
             radioStationNumber++;
         } else {
             radioStationNumber = 0;
         }
     }
+
     //предыдущий номер станции
     public void previousStationNumber() {
         if (radioStationNumber != 0) {
             radioStationNumber--;
         } else {
-            radioStationNumber = 9;
+            radioStationNumber = maxRadioStations;
         }
     }
+
     //узнать номер радиостанции
     public int getRadioStationNumber() {
         return radioStationNumber;
     }
+
     //установите номер радиостанции
     public void setRadioStationNumber(int radioStationNumber) {
         if (radioStationNumber < 0) {
             return;
         }
-        if (radioStationNumber > 9) {
+        if (radioStationNumber > maxRadioStations) {
             return;
         }
         this.radioStationNumber = radioStationNumber;
@@ -35,31 +54,33 @@ public class Radio {
 
     //увеличьте громкость звука
     public void increaseTheSoundVolume() {
-        if (soundVolume != 10) {
+        if (soundVolume != maxVolumeLevel) {
             soundVolume++;
         } else {
             soundVolume = 0;
         }
     }
+
     //уменьшите громкость звука
     public void reduceTheSoundVolume() {
         if (soundVolume != 0) {
             soundVolume--;
         } else {
-            soundVolume = 10;
+            soundVolume = maxVolumeLevel;
         }
     }
+
     //увеличьте громкость звука
     public int getSoundVolume() {
-
         return soundVolume;
     }
+
     //установите громкость звука
     public void setSoundVolume(int soundVolume) {
         if (soundVolume < 0) {
             return;
         }
-        if (soundVolume > 10) {
+        if (soundVolume > maxVolumeLevel) {
             return;
         }
         this.soundVolume = soundVolume;
--- src/test/java/org/example/Radioman/radio/RadioTest.java
+++ src/test/java/org/example/Radioman/radio/RadioTest.java
@@ -5,9 +5,67 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class RadioTest {
 
-    //номер радиостанции
+public class RadioTest {
+    //номер радиостанции в конструкторе с параметрами
+    @ParameterizedTest
+    @CsvSource({
+            "30,30",
+            "40,40",
+            "10,10",
+            "11,11",
+            "49,49",
+            "50,0",
+            "51,0",
+            "-50,0"
+    })
+    public void settingTheRadioStationNumber(int radioStationNumber, int expected) {
+        Radio service = new Radio(50);
+        service.setRadioStationNumber(radioStationNumber);
+
+        int actual = service.getRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //номер предыдущей радиостанции с параметрами
+    @ParameterizedTest
+    @CsvSource({
+            "0,49",
+            "49,48",
+            "10,9",
+            "11,10",
+            "25,24"
+    })
+    public void radioStationNumberWhenSwitching(int radioStationNumber, int expected) {
+        Radio service = new Radio(50);
+        service.setRadioStationNumber(radioStationNumber);
+
+        service.previousStationNumber();
+
+        int actual = service.getRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //номер следующей радиостанции с параметрами
+    @ParameterizedTest
+    @CsvSource({
+            "9,10",
+            "10,11",
+            "36,37",
+            "48,49",
+            "49,0"
+    })
+    public void theTextStationTheMaxRange(int radioStationNumber, int expected) {
+        Radio service = new Radio(50);
+        service.setRadioStationNumber(radioStationNumber);
+
+        service.nextStationNumber();
+
+        int actual = service.getRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //номер радиостанции без параметров
     @ParameterizedTest
     @CsvSource({
             "5,5",
@@ -28,7 +86,7 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    //следующая радиостанция
+    //следующая радиостанция без параметров
     @ParameterizedTest
     @CsvSource({
             "7,8",
@@ -48,7 +106,7 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    //предыдущая радиостанция
+    //предыдущая радиостанция без параметров
     @ParameterizedTest
     @CsvSource({
             "5,4",
@@ -71,14 +129,17 @@ public class RadioTest {
     //уровень громкости
     @ParameterizedTest
     @CsvSource({
-            "5,5",
-            "9,9",
+            "50,50",
+            "35,35",
             "10,10",
-            "11,0",
+            "100,100",
+            "99,99",
+            "101,0",
             "0,0",
             "1,1",
             "2,2",
-            "-1,0"
+            "-1,0",
+            "-100,0"
     })
     public void volumeLevel(int soundVolume, int expected) {
         Radio service = new Radio();
@@ -91,13 +152,16 @@ public class RadioTest {
     //увеличение громкости
     @ParameterizedTest
     @CsvSource({
-            "6,7",
+            "60,61",
             "0,1",
             "1,2",
             "2,3",
-            "8,9",
-            "9,10",
-            "10,0"
+            "54,55",
+            "99,100",
+            "100,0",
+            "10,11",
+            "15,16",
+            "9,10"
     })
     public void increaseTheVolume(int soundVolume, int expected) {
         Radio service = new Radio();
@@ -112,13 +176,14 @@ public class RadioTest {
     //уменьшение громкости
     @ParameterizedTest
     @CsvSource({
-            "5,4",
-            "0,10",
-            "1,0",
-            "2,1",
+            "45,44",
+            "0,100",
+            "100,99",
+            "99,98",
+            "20,19",
             "10,9",
             "9,8",
-            "8,7"
+            "88,87"
     })
     public void reducingTheSoundVolume(int soundVolume, int expected) {
         Radio service = new Radio();
